85510c442b19105bd0eab3a951af6e5f8f436f62
EEDP <edgarefraindp@gmail.com>
2017-02-20 06:00:12 -1200

    Clase Figura y Triangulo
    Se crea una clase base de la cual heredan todas las demas clases o
    figuras.

---
+++ ProjectoFiguras/Figura.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Figura here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Figura
+{
+   protected float area;
+   public Figura(){
+       area = 0;
+   }
+   
+   /*public void calculaArea(){
+       
+   }*/
+   
+   public float accedeArea(){
+       return area;
+   }
+}

fa285020a6fc9488776407e57c1884980c9b1653
Alejandro Esaú Alvarez <alvalex_@hotmail.com>
2017-02-20 17:12:54 -0600

    Nuevas Clases
    Se agrego la clase Circulo y la clase Cuadrado

---
+++ ProjectoFiguras/Circulo.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Circulo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Circulo extends Figura
+{
+    private float radio;
+    
+    public Circulo(float r){
+        radio = r;
+    }
+    
+    public void calculaArea(){
+        area = (float)(3.141592*radio*radio)/2;
+    }
+}

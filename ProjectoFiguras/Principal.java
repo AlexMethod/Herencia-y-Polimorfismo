8dde412e249624f60b60a0de7bddeada4a737886
Alejandro Esaú Alvarez <alvalex_@hotmail.com>
2017-02-21 15:33:14 -0600

    ArrayList, Iteradores, For mejorado
    En la clase Principal se implemento un ArrayList que tiene como
    elementos los objetos triangulo, circulo y cuadrado, se utilizo iterador
    para recorrer el arreglo y calcular el area y se utilizo el for mejorado

--- ProjectoFiguras/Principal.java
+++ ProjectoFiguras/Principal.java
@@ -5,26 +5,34 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.util.ArrayList;
+import java.util.Iterator;
 public class Principal
 {
    public static void main(String[]args){
        Triangulo t1 = new Triangulo(3,4);
        Cuadrado c1 = new Cuadrado(3);
        Circulo cc1 = new Circulo(3);
-       t1.calculaArea();
-       System.out.println(t1.accedeArea());
        
-       Figura []arr= new Figura[3];
-       arr[0]=t1;
-       arr[1]=c1;
-       arr[2]=cc1;
+       ArrayList<Figura> arr= new ArrayList<Figura>();;
+       arr.add(t1);
+       arr.add(c1);
+       arr.add(cc1);
        
-       for(int i = 0;i<arr.length; i++){
-           arr[i].calculaArea();
+       Iterator<Figura> iterador = arr.iterator();
+       
+       /*while(iterador.hasNext()){ //Calcular area con iterador
+          (iterador.next()).calculaArea();
        }
        
-       for(int j = 0;j<arr.length;j++){
-           arr[j].accedeArea();
-       }
+       while(iterador.hasNext()){ //Imprimir area con iterador
+          Figura f = iterador.next();
+          System.out.println(f.accedeArea()); 
+       }*/
+      
+      for(Figura fig:arr){ //For mejorado
+         fig.calculaArea();
+         System.out.println(fig.accedeArea());
+      }
    }
 }

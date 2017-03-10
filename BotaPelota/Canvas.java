f2d9ca53f71f2bbf8ce8cf8f992d19ec20ccb1a4
Alejandro Esau Alvarez <alvalex_@hotmail.com>
2017-03-10 06:12:22 -1200

    EscuchadorTeclado
    Se cambio el EscuchadorTiempo  a la clase Ventana y se implemento el
    EscuchadorTeclado

--- BotaPelota/Canvas.java
+++ BotaPelota/Canvas.java
@@ -7,28 +7,23 @@
  */
 import javax.swing.JPanel;
 import java.awt.Graphics;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.Timer;
-public class Canvas extends JPanel
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+public class Canvas extends JPanel 
 {
     private Pelota ball;
     public Canvas(){
         ball= new Pelota(100,100,10);
-        EscuchadorTiempo escuchaTiempo=new EscuchadorTiempo();
-        Timer tiempo = new Timer(1000,escuchaTiempo);
-        tiempo.start();
     }
-    class EscuchadorTiempo implements ActionListener{
-        @Override 
-        public void actionPerformed(ActionEvent e){
-            System.out.println("Hola");
-            /*ball.muevete();
-            this.repaint();*/
-        }
-    }
+   
     @Override 
-    public void paintComponent(Graphics g){
-        ball.dibujate(g);
+        public void paintComponent(Graphics g){
+            ball.dibujate(g);
+        }
+        
+    public void actua(){
+       // ball.muevete(this.getBounds());
+        this.repaint();
+        
     }
 }
--- BotaPelota/Ventana.java
+++ BotaPelota/Ventana.java
@@ -6,14 +6,54 @@
  * @version (a version number or a date)
  */
 import javax.swing.JFrame;
+import java.awt.event.KeyListener;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
+import java.awt.event.KeyEvent;
 public class Ventana extends JFrame
 {
     private Canvas dibujo;
+    private Timer tiempo;
     public Ventana(int ancho,int alto){
         this.setSize(ancho,alto);
         dibujo=new Canvas();
         this.add(dibujo);
+        EscuchadorTeclado escuchaTeclado = new EscuchadorTeclado();
+        this.addKeyListener(escuchaTeclado);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        EscuchadorTiempo escuchaTiempo=new EscuchadorTiempo();//determina la accion a ejecutar 
+        Timer tiempo = new Timer(1000,escuchaTiempo); //
+        tiempo.start();
+    }
+    
+    class EscuchadorTeclado implements KeyListener{
+        @Override
+        public void keyPressed(KeyEvent e){
+            if(e.getKeyChar()=='d'){
+               tiempo.stop();
+            }
+            if(e.getKeyChar()=='i'){
+                tiempo.start();
+            }
+            
+        }
+        @Override
+        public void keyReleased(KeyEvent e){
+            System.out.println("Key Released: "+e.getKeyChar());
+        }
+        @Override 
+        public void keyTyped(KeyEvent e){
+            System.out.println("Key Typed: "+e.getKeyChar());
+        }
+    }
+     class EscuchadorTiempo implements ActionListener{
+        @Override 
+        public void actionPerformed(ActionEvent e){
+            dibujo.actua();
+            dibujo.repaint();
+        }
     }
     
 }
